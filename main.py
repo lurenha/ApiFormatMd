11b2fab1c1286bd146a267383c433980438cb107
weipeng <1143038749@qq.com>
2022-08-08 22:35:24 +0800

    feat(): java接口转换为MarkDown V5

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import re
-import time
 
 # 作者
 author = '江火似流萤'
@@ -13,7 +12,8 @@ basic_type = set(
     ['BigDecimal', 'String', 'Timestamp', 'Long', 'Integer', 'Boolean', 'boolean', 'Map', 'List', 'BigInteger', 'T',
      'Date', 'long', 'int'])
 redo_type = set()
-dto_dic = {}
+java_class_dic = {}
+dto_dic_path = {}
 template_txt = '''
 ## 接口说明
 
@@ -42,57 +42,68 @@ template_txt = '''
 '''
 
 
-def generate_file_dfs(root_path=root_path):
+def find_all_java_file_path(cur_path):
     # 查找文件代码
-    files = os.listdir(root_path)
+    files = os.listdir(cur_path)
     for s in files:
-        s_path = os.path.join(root_path, s)
-        if os.path.isdir(s_path):
-            generate_file_dfs(s_path)
+        s_path = os.path.join(cur_path, s)
+        if os.path.isdir(s_path) and 'target' != s:
+            find_all_java_file_path(s_path)
         elif os.path.isfile(s_path):
-            cur = s.split(".")[0]
-            if cur in redo_type and cur not in dto_dic:
-                print("dic add:" + cur)
-                dto_dic[cur] = read_file(s_path)
+            pre = s.split(".")[0]
+            if '.java' in s:
+                dto_dic_path[pre] = s_path
 
 
-def read_file(file_path):
-    if file_path:
-        with open(file_path, encoding='utf-8') as file:
-            r = file.read()
-            return r
+def read_content_by_file_path(file_path):
+    if not file_path:
+        return None
+    with open(file_path, encoding='utf-8') as file:
+        r = file.read()
+        return r
 
 
 def beautify_enum(name_enum, content):
-    return '<h5 id="' + name_enum + '">' + name_enum + '</h5>\n\n```java\n' + content.split(name_enum + "(")[
-        0] + '}\n```\n'
+    template2 = '''<h5 id="{name_enum}">{name_enum}</h5>\n
+```java
+  \n
+{table}
+```\n'''
+    return template2.format(name_enum=name_enum, table=content.split(name_enum + "(")[0] + '}')
 
 
-def beautify_dto(name_dto, content):
+def beautify_class(name_class, content):
+    template1 = '''<h5 id="{name_class}">{name_class}</h5>\n\n
+|字段|类型|必填|示例|说明|
+| :----: | :----: | :----: | :----: | :----: |\n{table}'''
+
     class_split = content.split("static class")
     content = class_split[0]
     r = re.findall('ApiModelProperty.*?"(.*?)"', content)
-    x = re.findall('private(.*?);', content)
+    x = re.findall('(private|protected)(.*?);', content)
     for i in range(len(x) - 1, -1, -1):
-        if 'static' in x[i]:
-            print("delete:" + x[i])
+        if 'static' in x[i][1]:
+            print("delete line:" + x[i][1])
             x.pop(i)
 
-    res = '<h5 id="' + name_dto + '">' + name_dto + '</h5>\n\n' + '''
-|字段|类型|必填|示例|说明|
-| :----: | :----: | :----: | :----: | :----: |\n'''
+    table = ''
     for i in range(len(x)):
-        split = str.split(x[i].strip(), "=")[0]
+        split = str.split(x[i][1].strip(), "=")[0]
+        if not split:
+            continue
         tar = str.split(split.strip(), " ")
-        res += "|%s|%s|-|-|%s|\n" % (
-            tar[-1], format_type(tar[0]), r[i] if i < len(r) else '-')
+        table += "|%s|%s|-|-|%s|\n" % (
+            tar[-1], format_type(tar[-2]), r[i] if i < len(r) else '-')
+    table += '---\n\n'
+
     for i in range(1, len(class_split)):
-        y = re.search('(.*?)\{', class_split[i])
+        y = re.search('(.*?)(extends|\{)', class_split[i])
         if y:
-            if y.group(1).strip() not in dto_dic:
-                dto_dic[y.group(1).strip()] = class_split[i]
-                print("dic add:" + y.group(1).strip())
-    return res
+            if y.group(1).strip() not in java_class_dic:
+                java_class_dic[y.group(1).strip()] = class_split[i]
+                print("dic add inner class:" + y.group(1).strip())
+
+    return template1.format(name_class=name_class, table=table)
 
 
 def generate_request(param_list):
@@ -117,17 +128,17 @@ def generate_response(dto):
 
 
 def generate_enum(name_enum):
-    if name_enum in dto_dic:
-        return beautify_enum(name_enum, dto_dic[name_enum])
+    if name_enum in java_class_dic:
+        return beautify_enum(name_enum, java_class_dic[name_enum])
     else:
-        return 'None\n'
+        return 'None\n---\n\n'
 
 
-def generate_dto(name_dto):
-    if name_dto in dto_dic:
-        return beautify_dto(name_dto, dto_dic[name_dto])
+def generate_class(name_class):
+    if name_class in java_class_dic:
+        return beautify_class(name_class, java_class_dic[name_class])
     else:
-        return 'None\n'
+        return 'None\n---\n\n'
 
 
 def format_type(type):
@@ -156,15 +167,18 @@ def generate_other():
     over_type = set()
     res = ''
     while redo_type:
-        generate_file_dfs()
         redo_type_slave = redo_type.copy()
         redo_type.clear()
         for i in redo_type_slave - over_type:
+            if i not in java_class_dic:
+                java_class_dic[i] = read_content_by_file_path(dto_dic_path[i])
+            print("generate class:" + i)
             if "Enum" in i:
                 res += generate_enum(i)
             else:
-                res += generate_dto(i)
-        over_type = over_type & redo_type_slave
+                res += generate_class(i)
+            print("----------------------------")
+        over_type = over_type | redo_type_slave
     return res
 
 
@@ -173,11 +187,22 @@ def write_file(name, text):
         f.write(text)
 
 
-def run(source_txt):
-    method = re.search('@RequestMapping.*?RequestMethod\.(.*?)[,\s\)]', source_txt).group(1)
-    url = re.search('@RequestMapping.*?value.*?"(.*?)"', source_txt).group(1)
+def run(source_txt, des):
+    find_all_java_file_path(root_path)
+    r1 = re.search('@RequestMapping.*?RequestMethod\.(.*?)[,\s\)]', source_txt)
+    if r1:
+        method = r1.group(1)
+    else:
+        method = re.search('@(.*)?Mapping', source_txt).group(1)
+
+    r2 = re.search('@RequestMapping.*?value.*?"(.*?)"', source_txt)
+    if r2:
+        url = r2.group(1)
+    else:
+        url = re.search('Mapping.*?value.*?"(.*?)"', source_txt).group(1)
+
     describe = re.search('@ApiOperation.*?value.*?"(.*?)"', source_txt)
-    describe = describe.group(1) if describe else 'describe'
+    describe = describe.group(1) if describe else des
 
     # 请求参数说明
     param_list = re.findall('@RequestParam.*?"(.*?)".*?\)(.*?)[,\)]', source_txt)
@@ -191,7 +216,7 @@ def run(source_txt):
 
     request = generate_request(param_list)
     # 返回参数说明
-    res_dto = re.search('public(.*?)\(', source_txt).group(1)
+    res_dto = re.search('public(.*?)\(', source_txt).group(1).strip()
     res_dto = str.split(res_dto.strip(), " ")[0]
     response = generate_response(res_dto)
     # 补充实体说明
@@ -206,33 +231,17 @@ def run(source_txt):
 
 if __name__ == '__main__':
     param = '''
-        /**
-         * 企业端-融信-融信签发-获取特定使用渠道可占用的额度信息
-         *
-         * @param fkCorp          签条方（即企业ID）
-         * @param fkProductCcbscf 建信融通产品编号
-         * @param limitUseChannel 使用渠道
-         * @return
-         * @author 商梦德
-         */
-        @RequestMapping(value = "/v1/limits/ccbscfs/details/channels/can-occupy/list", method = RequestMethod.GET)
-        public List<CcbscfLimitDetailDTO> listCanOccupyCcbscfLimit(
-                @RequestParam(value = "fkCorp", required = true) String fkCorp,
-                @RequestParam(value = "fkProductCcbscf", required = true) String fkProductCcbscf,
-                @RequestParam(value = "limitUseChannel", required = true) LimitUseChannelEnum limitUseChannel) {
-            List<TLmtCcbscfDetail> ccbscfLimitDetails = limitAdapterService.listCanOccupyCcbscfLimit(fkCorp,
-                    fkProductCcbscf, limitUseChannel);
-            if (ccbscfLimitDetails != null) {
-                List<CcbscfLimitDetailDTO> ccbscfLimitDetailDTOs = BeanUtils.mapc(ccbscfLimitDetails,
-                        CcbscfLimitDetailDTO.class);
-                // 设置额度来源企业全称
-                ccbscfLimitDetailDTOs = limitCcbscfReleaseService.setLimitDetailSourceFullname(ccbscfLimitDetailDTOs);
-                return ccbscfLimitDetailDTOs;
-            }
-            return null;
-        }
+    @ApiOperation(value = "平台端根据流程编码查询认证准入信息", notes = "平台端根据流程编码查询认证准入信息")
+    @RequestMapping(value = "/v1/corp/real-name/auth/find", method = RequestMethod.GET)
+    public BizCorpRealNameAuthResponseVO findCorpRealName(@RequestParam(value = "fkRnProgress") String fkRnProgress) {
+        BizCorpRealNameAuthResponseDTO bizCorpRealNameAuthResponseDTO = corpRnAuthService.findCorpRealName(fkRnProgress).tryResult();
+        BizCorpRealNameAuthResponseVO bizCorpRealNameAuthResponseVO = BeanUtils.map(bizCorpRealNameAuthResponseDTO, BizCorpRealNameAuthResponseVO.class);
+        String currentNodeState = bizCorpRealNameAuthResponseVO.getCurrentNodeState();
+        String label = corpRnAuthService.calculateLabel(fkRnProgress, CurrentNodeStateEnum.valueOfCode(currentNodeState)).tryResult();
+        bizCorpRealNameAuthResponseVO.setLabel(label);
+        setDirectorsProperty(Optional.ofNullable(bizCorpRealNameAuthResponseVO).map(BizCorpRealNameAuthResponseVO::getRnCorp).orElseGet(() -> null));
+        return bizCorpRealNameAuthResponseVO;
+    }
     '''
 
-    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
-    run(param)
-    print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+    run(param, '是否可以占用额度')
